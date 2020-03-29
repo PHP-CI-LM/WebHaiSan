1044a73d56c100ec6713d09c76d5c99f6332b7c6
minhho98langlon <37680096+minhho98langlon@users.noreply.github.com>
2020-03-29 22:59:43 +0700

    Remove old scipt

--- scriptSQL/accounts.sql
+++
@@ -1,6 +0,0 @@
-/*
--- Query: SELECT * FROM webhaisan.accounts
--- Date: 2019-12-30 12:16
-*/
-INSERT INTO `` (`AccountID`,`UserName`,`Password`,`id_permission`) VALUES (1,'minhho98langlon','d86cb9e5c17a5ecf0c2def9c73f38788',1);
-INSERT INTO `` (`AccountID`,`UserName`,`Password`,`id_permission`) VALUES (2,'thuan1','c1921ad3b7cc07f38a81334814a99657',1);
