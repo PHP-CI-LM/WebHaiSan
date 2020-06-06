d12c8e9fa7bd85081700edf462dd7d1fa75a9c4e
minhho98langlon <37680096+minhho98langlon@users.noreply.github.com>
2020-06-06 12:45:10 +0700

    Update iframe

--- application/views/comp/Footer.php
+++ application/views/comp/Footer.php
@@ -2,6 +2,7 @@
 
 <!-- Load Facebook SDK for JavaScript -->
 <!-- Load Facebook SDK for JavaScript -->
+<!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
       <script>
         window.fbAsyncInit = function() {
@@ -15,17 +16,14 @@
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
       }(document, 'script', 'facebook-jssdk'));</script>
 
       <!-- Your Chat Plugin code -->
       <div class="fb-customerchat"
         attribution=setup_tool
-        page_id="100722644961244"
-  theme_color="#44bec7"
-  logged_in_greeting="Hi! How can we help you?"
-  logged_out_greeting="Hi! How can we help you?">
+        page_id="101485071604686">
       </div>
 <body>
     <script>
