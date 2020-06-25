09169350fbc61da4892d17c56b45b575c253787c
minhho98langlon <37680096+minhho98langlon@users.noreply.github.com>
2020-06-25 23:55:37 +0700

    Responsive top nav element in admin page

---
+++ application/views/admin/comp/topNav.php
@@ -0,0 +1,25 @@
+<div class="topNav" style="position: relative;">
+    <div class="wrapper">
+        <div class="welcome">
+            <span>Xin chào: <b>admin!</b></span>
+        </div>
+
+        <div class="userNav" style="margin-left: 214px; margin-right: 0px">
+            <ul>
+                <li><a href="http://localhost/webphp/" target="_blank">
+                        <img style="margin-top:7px;" src="<?php echo public_url() ?>/images/icons/light/home.png" />
+                        <span>Trang chủ</span>
+                    </a></li>
+
+                <!-- Logout -->
+                <li><a href="<?php echo base_url() ?>ci-admin/logout.html">
+                        <img src="<?php echo public_url() ?>/images/icons/topnav/logout.png" alt="" />
+                        <span>Đăng xuất</span>
+                    </a></li>
+
+            </ul>
+        </div>
+
+        <div class="clear"></div>
+    </div>
+</div>
--- application/views/admin/user.php
+++ application/views/admin/user.php
@@ -69,32 +69,7 @@
 	<div id="rightSide">
 
 		<!-- Account panel top -->
-
-		<div class="topNav">
-			<div class="wrapper">
-				<div class="welcome">
-					<span>Xin chào: <b>admin!</b></span>
-				</div>
-
-				<div class="userNav">
-					<ul>
-						<li><a href="" target="_blank">
-								<img style="margin-top:7px;" src="<?php echo public_url() ?>/images/icons/light/home.png" />
-								<span>Trang chủ</span>
-							</a></li>
-
-						<!-- Logout -->
-						<li><a href="<?php echo base_url()?>ci-admin/logout.html">
-								<img src="images/icons/topnav/logout.png" alt="" />
-								<span>Đăng xuất</span>
-							</a></li>
-
-					</ul>
-				</div>
-
-				<div class="clear"></div>
-			</div>
-		</div>
+		<?php require("comp/topNav.php")?>
 
 		<!-- Main content -->
 		<!-- Common view -->
