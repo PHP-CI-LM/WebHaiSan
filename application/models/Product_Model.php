a76a17f70c463413622893898e82b5ac170d143c
vophuocson <vophuocson.1998@gmail.com>
2020-08-04 12:16:38 +0700

    update function enterwordsearch

--- application/models/Product_Model.php
+++ application/models/Product_Model.php
@@ -98,9 +98,9 @@ class Product_Model extends CI_Model
 
     public function getProductsWithName($name, $limit = 0, $start = -1)
     {
-        $query = "SELECT p.id_product, p.name_product, p.price, p.descript, p.importDate, p.count_view, p.image_link As DuongDan, c.name_category, p.discount, p.count_buy, o.name_origin, p.size, u.name_unit
+        $query = "SELECT p.id_product, p.name_product, p.price, p.descript, p.importDate, p.count_view, p.image_link As DuongDan, c.name_category, p.discount, p.count_buy, o.name_origin, p.size, u.name_unit,p.count_view
             FROM products As p, categories As c, units As u, origins As o
-            WHERE p.id_category = c.id_category And p.id_origin = o.id And p.id_unit = u.id_unit And UPPER(p.name_product) Like UPPER('%".$name."%')";
+            WHERE p.id_category = c.id_category And p.id_origin = o.id And p.id_unit = u.id_unit And UPPER(p.name_product) Like UPPER('%".$name."%') order by p.count_view DESC";
         if ($limit !== 0 && $start != -1) {
             $query = $query.' LIMIT '. $start . ', ' . $limit;
         } else if ($limit != 0) {
